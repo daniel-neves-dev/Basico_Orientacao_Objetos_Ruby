970cc4eed3f7b4f5a88313288ccf8cf0f4967c60
Daniel Neves Oliveira <daniel.oliveira.au@hotmail.com>
2022-11-15 12:57:53 -0300

    Update variavel_classe.rb

--- variavel_classe.rb
+++ variavel_classe.rb
@@ -1,9 +1,31 @@
+#class Pessoa
+#    def self.gerar
+#        p"gerando..." 
+#        Pessoa.new
+#    end
+#end
+
+#pessoa = Pessoa.gerar #chamar metodo de classe
+#p pessoa
+
 class Pessoa
+    @@numero_pessoas = 0 #variavel de classe
+
     def self.gerar
-        p"gerando..." 
-        Pessoa.new
+        @@numero_pessoas += 1
+        p "gerando..."
+        Pessoa.new 
     end
+
+    def self.numero_pessoas
+        @@numero_pessoas
+    end
+
 end
 
-pessoa = Pessoa.gerar #chamar metodo de classe
-p pessoa
+pessoa = Pessoa.gerar
+pessoa = Pessoa.gerar
+pessoa = Pessoa.gerar
+p Pessoa.numero_pessoas
+
+# Variavel de classe tem efeito global, por isso não é muito utilizado
